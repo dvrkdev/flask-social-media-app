bd92455aff0c681baa34038c8b22205ba920f58b
dvrkdev <dvrk.devx@gmail.com>
2025-12-10 05:12:01 +0500

    feat: home page

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,22 @@
 # main.py
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import current_user, login_required
+
+from app import db
+from app.forms import PostForm
+from app.models import Post
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    posts = Post.query.order_by(Post.created_at.desc()).all()
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(content=form.content.data, user_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash("Post muvaffaqiyatli yaratildi!", "success")
+        return redirect(url_for("main.home"))
+    return render_template("home.html", form=form, posts=posts)
